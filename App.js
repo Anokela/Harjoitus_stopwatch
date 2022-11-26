aaf9db15f6eb4edcaef451c103eaf8484edea316
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:14:29 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Clock from './Components/Clock.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Clock></Clock>
     </View>
   );
 }
@@ -18,3 +17,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
